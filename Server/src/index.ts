e453d17d6043a15e482ad9b0d469952bb19ef6a1
avenmia <mavend2@gmail.com>
2020-02-06 19:11:31 -0600

    added eslint

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import * as WebSocket from "ws";
+import * as http from "http";
+
 const app = express();
 const port = 8080; // default port to listen
 
@@ -6,6 +9,25 @@ const port = 8080; // default port to listen
 app.get( "/", ( req, res ) => {
     res.send( "Hello world!" );
 } );
+
+const server = http.createServer(app);
+
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws: WebSocket) => {
+
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message: string) => {
+
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+
+    //send immediatly a feedback to the incoming connection    
+    ws.send('Hi there, I am a WebSocket server');
+});
 
 // start the Express server
 app.listen( port, () => {
