234eabfb9aa6a993ee92e841feadec8deacb9f1b
avenmia <mavend2@gmail.com>
2020-02-20 22:33:31 -0600

    extracted auth method to new folder and changed init to auth enum type

---
+++ Server/src/EventHandlers.ts
@@ -0,0 +1,27 @@
+import SharedSecret from "./Secret";
+import { Client } from "./Clients";
+
+export function onAuthencation(payload:any, num:number, ws:any, people: Client[]) {
+    console.log(payload);
+    console.log(SharedSecret);
+    if (SharedSecret === payload.secret) {
+      const client = new Client("client" + num.toString(), ws);
+      num = num++;
+      people.push(client);
+      people.forEach(p => console.log(`person name ${p.username}`));
+    } else {
+      ws.send(ClientResponse("error", "Invalid token"));
+      ws.close();
+    }
+}
+
+export function onInit(payload:any){
+  console.log("Init received")
+}
+
+export function ClientResponse(type: string, message: any){
+    return JSON.stringify({
+      type,
+      message
+    });
+  };
---
+++ Server/src/Secret.ts
@@ -0,0 +1,8 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+const SharedSecret = process.env.SHARED_SECRET;
+if (!SharedSecret) {
+    console.error("Set the SHARED_SECRET environment variable");
+    process.exit(1);
+  }
+export default SharedSecret;
