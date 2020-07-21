1f3f1c9f0021bc39c6215d4e1e0b42292da04889
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2020-07-20 21:41:57 -0500

    tag syntax fix

--- client/setup.sh
+++ client/setup.sh
@@ -2,19 +2,24 @@
 
 apt-get update
 apt-get install -y curl
+hostnamectl set-hostname quantum-lamps-1
 raspi-config nonint get_i2c
 raspi-config nonint do_i2c 0
 curl -fsSL https://get.docker.com | sh
-
-curl -fsSL -o master.zip https://github.com/avenmia/quantum-lamps/archive/master.zip
-unzip master.zip
-rm master.zip
-cd quantum-lamps-master/client
-docker build -t quantum-lamps-client .
 
 cat << EOF > .env
 WS_URL=
 SHARED_SECRET=
 EOF
 
-docker run --privileged --net host -d --restart unless-stopped -v $(pwd)/.env:/app/.env quantum-lamps-client
+docker login docker.pkg.github.com
+
+docker run \
+    --privileged \
+    --net host \
+    -d \
+    --restart unless-stopped \
+    -v $(pwd)/.env:/app/.env \
+    docker.pkg.github.com/avenmia/quantum-lamps/client:v6
+
+reboot
