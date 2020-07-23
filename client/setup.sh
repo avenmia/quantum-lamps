4cc1b6c1a9c9150586c10887aa27e7777893c5f5
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2020-07-22 22:34:17 -0500

    Update setup.sh

--- client/setup.sh
+++ client/setup.sh
@@ -7,19 +7,13 @@ raspi-config nonint get_i2c
 raspi-config nonint do_i2c 0
 curl -fsSL https://get.docker.com | sh
 
-cat << EOF > .env
-WS_URL=
-SHARED_SECRET=
-EOF
-
-docker login docker.pkg.github.com
-
 docker run \
     --privileged \
     --net host \
     -d \
     --restart unless-stopped \
-    -v $(pwd)/.env:/app/.env \
-    docker.pkg.github.com/avenmia/quantum-lamps/client:v6
+    -e WS_URL=ws://quantum-lamps-server \
+    -e SHARED_SECRET=secret \
+    avenmia/quantum-lamps-client:v0.6
 
 reboot
